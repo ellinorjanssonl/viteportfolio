e805f597bb7a3649978c21be1ea6e8b3e29c5cde
ellinor Jansson lande <ellinor_jansson@hotmail.com>
2024-02-16 18:25:08 +0100

    new commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import styles from "./App.module.css";
-import {Navbar} from "./components/Navbar/Navbar";
+import { Navbar } from "./components/Navbar/Navbar";
 import { About } from "./components/About/About";
 import { Skills } from "./components/Skills/Skills";
 import { Projects } from "./components/Projects/Projects";
 import { Contact } from "./components/Contact/Contact";
 import "bootstrap/dist/css/bootstrap.min.css";
+/* import { motion } from "framer-motion" */
 
 
 
--- src/components/Skills/Skills.jsx
+++ src/components/Skills/Skills.jsx
@@ -1,51 +1,39 @@
 import React from "react";
 import styles from "./Skills.module.css";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faGithub, faGit, faNodeJs, faHtml5, faCss3Alt, faJs, faReact, faBootstrap } from "@fortawesome/free-brands-svg-icons";
 
 export const Skills = () => {
-    return <div id="skills" className={styles.skills}>
-            
-            
-            <div className={styles.pContainer}>
-            <div className={styles.diamond}>
-            <img src="/Illustration (1).png" alt="diamond" />
-            </div>
-            <p className={styles.p}>Here are some of my different experiences in 
-            frontend, backend and other things I consider important for a good understanding of web-aplication designs.</p>
-            </div>
-
-            <div className={styles.skillsContainer}>
-                <div className={styles.skill}>
-                    <h2 className ={styles.h2}>Frontend</h2>
-                    <ul className ={styles.list}>
-                        <li>HTML</li>
-                        <li>CSS</li>
-                        <li>JavaScript</li>
-                        <li>React</li>
-                        <li>Bootstrap</li>
-                    </ul>
+    return (
+        <div id="skills" className={styles.skills}>
+            <div className={styles.flexContainer}>
+                <div className={styles.pContainer}> 
+                 <p className={styles.p}>To the right is a list of my different experiences in 
+                    frontend, backend and other things I consider important for a good understanding of web-aplication designs.</p>
                 </div>
-                <div className={styles.skill}>
-                <h2 className ={styles.h2}>Backend</h2>
-                    <ul className ={styles.list}>
-                        <li>Node.js</li>
-                        <li>Express</li>
-                        <li>API</li>
-                        <li>SQL</li>
-                    </ul>
+                <div className={styles.diamond}>
+                <img src="/Illustration (1).png" alt="diamond" />
                 </div>
-                <div className={styles.skill}>
-                <h2 className ={styles.h2}>Other</h2>
-                    <ul className ={styles.list}>
-                        <li>Git</li>
-                        <li>GitHub</li>
-                        <li>Photoshop</li>
-                        <li>Illustrator</li>
-                    </ul>
+                <div className={styles.skillsContainer}>
+                    <div className={styles.skill}>
+                        <ul className={styles.list}>
+                            <li><FontAwesomeIcon icon={faHtml5} size="lg" color="red" />Html</li>
+                            <li><FontAwesomeIcon icon={faCss3Alt} size="lg" color="#4487d4" />Css</li>
+                            <li><FontAwesomeIcon icon={faJs} size="lg" color="yellow" />Javascript</li>
+                            <li><FontAwesomeIcon icon={faReact} size="lg" color="#3997e8" />React</li>
+                            <li><FontAwesomeIcon icon={faBootstrap} size="lg"color="#43239b" />Bootstrap</li>
+                            <li><FontAwesomeIcon icon={faNodeJs} size="lg" color="#3ab13a"/>NodeJs</li>
+                            <li>Express</li>
+                            <li>API</li>
+                            <li>SQL</li>
+                            <li><FontAwesomeIcon icon={faGit} size="lg" color="red" /> Git</li>
+                            <li><FontAwesomeIcon icon={faGithub} size="lg" /> Github</li>
+                            <li><FontAwesomeIcon icon={faGithub} size="lg" /> Photoshop </li> 
+                        </ul>
+                    </div>
                 </div>
-            
-                <div className={styles.imageBackground2}>
-         <img src="/Background2.png" alt="bakgrund" />
-         </div>
             </div>
-        </div>;
-    };
+        </div>
+    );
+}
