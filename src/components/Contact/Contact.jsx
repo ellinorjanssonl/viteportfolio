6ecd5b1e78ab16ce650c1428cc9c2610b2e5b55f
ellinor Jansson lande <ellinor_jansson@hotmail.com>
2024-02-20 22:00:56 +0100

    contact form

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -6,6 +6,7 @@ import { faLinkedin, faGithub, faFacebook } from '@fortawesome/free-brands-svg-i
 
 export const Contact = () => {
   const [comment, setComment] = useState(""); // Hantera användarens kommentar
+  const [formVisible, setFormVisible] = useState(false); // State för att hantera formulärets synlighet
 
   // Hantera formulärets submit-händelse
   const handleSubmit = (e) => {
@@ -15,7 +16,7 @@ export const Contact = () => {
     const form = new FormData();
     form.append("message", comment);
 
-    fetch("https://formspree.io/f/myyrwayj", { // Ersätt 'yourFormId' med din faktiska Formspree ID
+    fetch("https://formspree.io/f/myyrwayj", {
       method: "POST",
       body: form,
       headers: {
@@ -24,7 +25,7 @@ export const Contact = () => {
     })
     .then(response => {
       if (response.ok) {
-        response.status(200).send("Thank you!");
+        alert("Thank you!");
         setComment(""); // Återställer kommentarfältet
       } else {
         alert("Please, try again!");
@@ -32,47 +33,56 @@ export const Contact = () => {
     });
   };
 
+  // Funktion för att växla synligheten av formuläret
+  const toggleFormVisibility = () => {
+    setFormVisible(!formVisible);
+  };
+
   return (
-    <motion.div
-      id="contact"
-      className={styles.contactContainer}
-      initial={{ opacity: 0, y: 20 }}
-      animate={{ opacity: 1, y: 0 }}
-      transition={{ duration: 0.5 }}
-    >
-      <h1 className={styles.contactHeader}>Kontakta Mig</h1>
-      <motion.div
-        className={styles.contactInfo}
-        initial={{ opacity: 0 }}
-        animate={{ opacity: 1 }}
-        transition={{ delay: 0.5 }}
-      >
-        <p><strong>E-post:</strong> Ellinor_jansson@hotmail.com</p>
-        <p><strong>Telefon:</strong> +46 707836682</p>
+    <div id="contact" className={styles.contact}>
+   
+      <h1 className={styles.contactHeader}>Contact me!</h1>
+      <p><strong>E-mail:</strong> Ellinor_jansson@hotmail.com</p>
+      <p><strong>Phone number:</strong> +46 707836682</p>
 
-        <div className={styles.socialMediaLinks}>
-  <a href="https://www.linkedin.com/in/dinprofil" target="_blank" rel="noopener noreferrer">
-    <FontAwesomeIcon icon={faLinkedin} size="lg" />
-  </a>
-  <a href="https://www.github.com/dinanvändarnamn" target="_blank" rel="noopener noreferrer">
-    <FontAwesomeIcon icon={faGithub} size="lg" />
-  </a>
-  <a href="https://www.facebook.com/dinprofil" target="_blank" rel="noopener noreferrer">
-    <FontAwesomeIcon icon={faFacebook} size="lg" />
-  </a>
-</div>
+      <div className={styles.socialMediaLinks}>
+        <a href="https://www.linkedin.com/in/dinprofil" target="_blank" rel="noopener noreferrer">
+          <FontAwesomeIcon icon={faLinkedin} size="lg" />
+        </a>
+        <a href="https://www.github.com/dinanvändarnamn" target="_blank" rel="noopener noreferrer">
+          <FontAwesomeIcon icon={faGithub} size="lg" />
+        </a>
+        <a href="https://www.facebook.com/dinprofil" target="_blank" rel="noopener noreferrer">
+          <FontAwesomeIcon icon={faFacebook} size="lg" />
+        </a>
+        {!formVisible && ( // Button is only shown if formVisible is false
+        <button onClick={toggleFormVisibility} className={styles.contactButton}>Want to leave a comment?</button>
+      )}
+      </div>
 
-    </motion.div><form className={styles.contactForm} onSubmit={handleSubmit}>
-        <label htmlFor="userComment">Din Kommentar</label>
-        <textarea
-          id="userComment"
-          name="comment"
-          rows="4"
-          placeholder="Skriv din kommentar här..."
-          value={comment}
-          onChange={(e) => setComment(e.target.value)}></textarea>
-        <button type="submit">Skicka Kommentar</button>
-      </form>
-    </motion.div>
+     
+
+      {formVisible && (
+        <motion.div
+          className={styles.contactFormContainer}
+          initial={{ scale: 0.9, opacity: 0 }}
+          animate={{ scale: 1, opacity: 2 }}
+          transition={{ duration: 1.5 }}
+        >
+          <form className={styles.contactForm} onSubmit={handleSubmit}>
+            <label htmlFor="userComment">Leave a comment!</label>
+            <textarea
+              id="userComment"
+              name="comment"
+              rows="4"
+              placeholder="Write your comment here..."
+              value={comment}
+              onChange={(e) => setComment(e.target.value)}
+            ></textarea>
+            <button type="submit">Send</button>
+          </form>
+        </motion.div>
+      )}
+    </div>
   );
 };
